be8567ba38d276e703dfff4783f5157caeef7b43
roman-oravec <romanoravec1@gmail.com>
2023-10-22 00:47:02 +0200

    notes error message

--- part1/notes/src/App.js
+++ part1/notes/src/App.js
@@ -1,12 +1,26 @@
 import { useState, useEffect } from 'react'
-//import axios from 'axios'
+
 import Note from './components/Note'
 import noteService from './services/notes'
+import './index.css'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -36,14 +50,22 @@ const App = (props) => {
   }
 
   const toggleImportanceOf = (id) => {
-    //console.log(`The importance of note ${id} needs to be toggled`)
-    //const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, import: !note.import } // object spreding - creates copy
 
-    noteService.update(id, changedNote).then(returnedNote => {
-      setNotes(notes.map(n => n.id !== id ? n : returnedNote))
-    })
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
@@ -51,9 +73,12 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
-      <button onClick={() => { setShowAll(!showAll) }}>
-        show {showAll ? 'important' : 'all'}
-      </button>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => { setShowAll(!showAll) }}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {notesToShow.map(note =>
           <Note
